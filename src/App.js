cbb267375249663b6e837b8ccad6f516a890769f
Shavkat Kasymov <kshavkatg@gmail.com>
2021-04-14 17:28:31 +0600

    New custom hook and adding new color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react'
 import colorData from "./color-data.json"
 import ColorList from "./ColorList"
+import AddColorForm from "./AddColorForm"
+import { v4 } from "uuid"
 
 export default function App() {
   const [colors, setColors] = useState(colorData)
   return (
     <>
+      <AddColorForm 
+        onNewColor={ (title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color
+            }
+          ]
+          setColors(newColors)
+        } }
+      />
       <ColorList 
         colors={colors} 
         onRateColor = { (id, rating) => {
