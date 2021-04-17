0251625c8c0ba35d893d2e756f72b8b5ae938f40
Shavkat Kasymov <kshavkatg@gmail.com>
2021-04-17 19:48:26 +0600

    React context

--- src/ColorList.js
+++ src/ColorList.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ColorContext } from "./"
 import Color from "./Color"
 
-export default function ColorList( { colors = [], onRemoveColor, onRateColor } ) {
+export default function ColorList() {
+  const { colors } = useContext(ColorContext)
   if(!colors.length) return <div>No Colors Listed</div>
   return (
     <div>
-      { colors.map(color => <Color key={colors.id} {...color} onRemove={onRemoveColor} onRate={onRateColor} />) }
+      { colors.map(color => <Color key={colors.id} {...color} />) }
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import colors from "./color-data.json"
 import ReactDOM from 'react-dom';
 import App from './App';
 
+export const ColorContext = createContext()
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ColorContext.Provider value={{colors}} >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ColorContext.Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
