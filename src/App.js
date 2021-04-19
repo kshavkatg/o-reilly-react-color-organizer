b846bdf98b51044ba234a1be538fe7082bc233bd
Shavkat Kasymov <kshavkatg@gmail.com>
2021-04-19 11:52:31 +0600

    adding and testing a useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ColorList from "./ColorList"
 import AddColorForm from "./AddColorForm"
+import {useAnyKeyToRender} from "./useAnyKeyToRender"
 
 export default function App() {
+  useAnyKeyToRender()
+
+  useEffect(() => {
+    console.log("fresh render")
+  })
+  
   return (
     <>
       <AddColorForm />
